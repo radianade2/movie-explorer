92561e867cc33030ee66d1f2b5f99dc97497648d
RahmiAulia <rahmiauliiaaa@gmail.com>
2024-09-30 14:08:16 +0700

    penggunaan tanstack query

--- src/components/TVShowTableTopRated.tsx
+++ src/components/TVShowTableTopRated.tsx
@@ -8,6 +8,7 @@ import {
 import { fetchGenres, fetchTopRatedTV } from "../api/apiConfig";
 import axios from "axios";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import { useTopRatedTV } from "../api/fetchingData";
 
 interface User {
   poster_path: string;
@@ -74,8 +75,10 @@ const TVShowTableTopRated = () => {
   const [tvShows, setTV] = useState<User[]>([]);
   const [genres, setGenres] = useState<{ [key: number]: string }>({});
   const [page, setPage] = useState(1); // Pagination state
-  const [totalPages, setTotalPages] = useState(1); // To track total pages from the API
-  const [isLoading, setIsLoading] = useState(false);
+
+  const {data, isLoading} = useTopRatedTV(page);
+  console.log(data);
+
 
   // Helper function untuk load likes dari localStorage
   const loadLikesFromLocalStorage = () => {
@@ -86,52 +89,6 @@ const TVShowTableTopRated = () => {
   // Helper function untuk menyimpan likes ke localStorage
   const saveLikesToLocalStorage = (likes: { [key: number]: number }) => {
     localStorage.setItem("likes", JSON.stringify(likes));
-  };
-
-  useEffect(() => {
-    const fetchAllData = async () => {
-      try {
-        setIsLoading(true);
-        const fetchedGenres = await fetchGenres();
-        const genreMap = fetchedGenres.reduce(
-          (
-            acc: { [key: number]: string },
-            genre: { id: number; name: string }
-          ) => {
-            acc[genre.id] = genre.name;
-            return acc;
-          },
-          {}
-        );
-        setGenres(genreMap);
-
-        // Fetching halaman pertama dari top-rated tv shows
-        await fetchTV(page);
-      } catch (error) {
-        console.error("Error fetching data", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchAllData();
-  }, [page]);
-
-  const fetchTV = async (page: number) => {
-    try {
-      const fetchedTV = await fetchTopRatedTV(page); // Pass page and limit 5 items
-      const savedLikes = loadLikesFromLocalStorage();
-
-      // Update movies dengan likes dari localStorage
-      const tvWithLikes = fetchedTV.results.map((tvShows: User) => ({
-        ...tvShows,
-        like: savedLikes[tvShows.id] !== undefined ? savedLikes[tvShows.id] : 0, // Menggunakan like yang sudah ada atau default ke 0
-      }));
-
-      setTV(tvWithLikes); 
-      setTotalPages(fetchedTV.total_pages); // Set total pages from API response
-    } catch (error) {
-      console.error("Error fetching movies", error);
-    }
   };
 
 
@@ -149,15 +106,15 @@ const TVShowTableTopRated = () => {
   };
 
   const table = useReactTable({
-    data: tvShows,
+    data: data?.results??[],
     columns: columns(genres, handleLike),
     getCoreRowModel: getCoreRowModel(),
     manualPagination: true, // Enable manual pagination
-    pageCount: totalPages,
+    pageCount: data?.total_pages,
   });
 
   const nextPage = () => {
-    if (page < totalPages) {
+    if (page < data?.total_pages) {
       setPage((prev) => prev + 1);
     }
   };
@@ -204,8 +161,8 @@ const TVShowTableTopRated = () => {
           {" "}
           {"<"}{" "}
         </button>
-        <span>{`Page ${page} of ${totalPages}`}</span>
-        <button onClick={nextPage} disabled={page === totalPages}>
+        <span>{`Page ${page} of ${data?.total_pages}`}</span>
+        <button onClick={nextPage} disabled={page === data?.total_pages}>
           {">"}
         </button>
       </div>
